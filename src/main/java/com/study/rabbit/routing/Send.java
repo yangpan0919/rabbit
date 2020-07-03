0ee541236b75bee8636b3095d347e9deb3aa3f58
杨攀 <473075889>
2020-07-03 16:51:13 +0800

    添加配置项说明

--- src/main/java/com/study/rabbit/routing/Send.java
+++ src/main/java/com/study/rabbit/routing/Send.java
@@ -8,15 +8,14 @@ import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
 public class Send {
-    private static final String EXCHANGE_NAME = "first_exchange_direct";
+    private static final String EXCHANGE_NAME = "test";
 
     public static void main(String[] args) throws IOException, TimeoutException {
         Connection connection = ConnectionUtils.getConnection();
         Channel channel = connection.createChannel();
-        channel.exchangeDeclare(EXCHANGE_NAME, "direct");
-
-        String msg = "hello direct";
-        String routingKey = "info";
+        channel.exchangeDeclare(EXCHANGE_NAME, "direct", true);
+        String msg = "这解释隔离。、！@#￥%……fslkdjf";
+        String routingKey = "127.0.0.2";
         channel.basicPublish(EXCHANGE_NAME, routingKey, null, msg.getBytes());
         channel.close();
         connection.close();
